0cff0cf0c64da79f97aac8591c7cc657be874a89
SpektralDevelopment <spektraldevelopment@gmail.com>
2014-03-04 19:44:35 -0500

    Implemented instance of BatteryStatus.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-var batteryStatus = new BatteryStatus();
+var
+    batteryStatus = new BatteryStatus(),
+    batteryLevel = batteryStatus.getBatteryLevel(),
+    isCharging = batteryStatus.isCharging(),
+    chargingTime = batteryStatus.getChargingTime(),
+    dischargingTime = batteryStatus.getDischargingTime();
+
+console.log("batteryLevel: " + batteryLevel);
+console.log("isCharging: " + isCharging);
+console.log("chargingTime: " + chargingTime);
+console.log("dischargingTime: " + dischargingTime);
+
+batteryStatus.onChargingChange(chargingChange);
+
+function chargingChange(evt) {
+    isCharging = batteryStatus.isCharging();
+    console.log("chargingChange: " + isCharging);
+}
 
 batteryStatus.onLevelChange(batteryLevelChange);
 
-function batteryLevelChange() {
-
+function batteryLevelChange(evt) {
+    console.log("batteryLevelChange: evt: " + evt);
 }
+
+
